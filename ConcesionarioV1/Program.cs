4eeb411c8d1fe944fa60d111b70821ab4f45573a
Carlos Ortiz Santiago <hecarlos123456@gmail.com>
2024-01-29 12:04:48 +0100

    Agregar archivos de proyecto.

---
+++ ConcesionarioV1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConcesionarioV1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CONTROLADOR PRINCIPAL DE LA APLICACIÓN
+
+            //MENU DE ACCIONES
+
+
+            //PROCESAMIENTO DE LAS ACCIONES
+        }
+    }
+}
