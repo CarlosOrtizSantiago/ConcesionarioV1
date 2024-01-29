930214504ef688483475de52b4a9bfe1311ed373
Carlos Ortiz Santiago <hecarlos123456@gmail.com>
2024-01-29 12:37:12 +0100

    Clase vehiculo: MIEMBROS PRIVADOS Y PROPIEDADES Y CONTRUCTORES

--- ConcesionarioV1/Vehiculo.cs
+++ ConcesionarioV1/Vehiculo.cs
@@ -12,13 +12,30 @@ namespace ConcesionarioV1
 
 
         //MIEMBROS PRIVADOS
-
+        private string _marca;
+        private string _modelo;
 
         //CONSTRUCTORES
-
+        public Vehiculo(string marca, string modelo) 
+        {
+            Marca = marca;
+            Modelo = modelo;
+        }
 
         //PROPIEDADES
+        public string Marca 
+        { 
+            get { return _marca; }
+            //TODO Proporcionar seguridad y proteccion al miembro _marca
+            set { _marca = value; }
+        }
 
+        public string Modelo
+        {
+            get { return _modelo; }
+            //TODO Proporcionar seguridad y proteccion al miembro _modelo
+            set { _modelo = value; }
+        }
 
         //METODOS PUBLICOS
 
